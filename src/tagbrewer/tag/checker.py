769b557b6569cfed5f7bc78a0e2e81296104d204
MVCowley <51127523+MVCowley@users.noreply.github.com>
2024-02-22 13:57:05 +0000

    Fixed find_gene_differences and added checker tests

--- src/tagbrewer/tag/checker.py
+++ src/tagbrewer/tag/checker.py
@@ -8,13 +8,14 @@
 from tagbrewer.tag import generators
 from tagbrewer.utils import categories
 import collections
+import pathlib
 
 def collapse_alleles(alleles_dict: dict) -> list:
     return [i for i in alleles_dict.keys()]
 
 def extract_gene_list(directory: str, species: str, version: str, gene_groups: str) -> set:
 
-    filename = f"{directory}{species}_{version}_{gene_groups}.tags"
+    filename = pathlib.Path(f"{directory}/{species}_{version}_{gene_groups}.tags").resolve()
 
     with open(filename, "r") as file:
         lines = [line.rstrip() for line in file]
@@ -22,7 +23,7 @@ def extract_gene_list(directory: str, species: str, version: str, gene_groups: s
     gene_names = []
     for line in lines:
         if version == "original":
-            gene_names.append(",".join(line.split("|")[1].split("/")))
+            gene_names.append("/".join(line.split("|")[1].split(",")))
         elif version == "extended":
             gene_names.append(line.split("|")[1])
         else:
@@ -31,7 +32,8 @@ def extract_gene_list(directory: str, species: str, version: str, gene_groups: s
     
     return set(gene_names)
 
-def find_gene_differences(directory: str, species: str, version: str) -> set:
+def find_gene_differences(directory: str, species: str, version: str, gene_group: str) -> set:
+    """ Takes an """
     if species == "human":
         species_fmt1 = "Homo sapiens"
         species_fmt2 = "human"
@@ -39,50 +41,15 @@ def find_gene_differences(directory: str, species: str, version: str) -> set:
         species_fmt1 = "Mus musculus"
         species_fmt2 = "mouse"
 
-    missing_genes = {}
-    for gene_group in categories.get_tr_gene_groups():
-            try:
-                alleles_functionality, alleles_fastas = generators.get_tr_alleles_for_gene_group_for_species(gene_group, species_fmt1)
-                imgt_gene_list = set(collapse_alleles(alleles_fastas))
-                dcr_gene_list = extract_gene_list(directory, species_fmt2, version, gene_group)
-                # TODO: fix diff bug
-                print(gene_group)
-                print(imgt_gene_list)
-                print(dcr_gene_list)
-                # diff = dcr_gene_list - set(imgt_gene_list)
-                diff = dcr_gene_list.difference(imgt_gene_list)
-                print(diff)
-                if len(diff) == 0:
-                    missing_genes[gene_group] = None
-                else:
-                    missing_genes[gene_group] = diff
-            except:
-                missing_genes[gene_group] = "Not present in current decombinator"
-
-    return missing_genes
-
-def find_taggable_gene_differences(directory: str, species: str, version: str, tag_len=20) -> set:
-    if species == "human":
-        species_fmt1 = "Homo sapiens"
-        species_fmt2 = "human"
-    elif species == "mouse":
-        species_fmt1 = "Mus musculus"
-        species_fmt2 = "mouse"
-
-    missing_genes = {}
-    for gene_group in categories.get_tr_gene_groups():
-            try:
-                alleles_functionality, alleles_fastas = generators.get_tr_alleles_for_gene_group_for_species(gene_group, species_fmt1)
-                gene_group_tags = generators.gen_tags(alleles_fastas, tag_len)
-                unique_tags = generators.find_unique_tags(gene_group_tags)
-                undecombinable_genes = generators.find_undecombinable_genes(alleles_fastas, unique_tags)
-                dcr_gene_list = extract_gene_list(directory, species_fmt2, version, gene_group)
-                diff = dcr_gene_list - set(unique_tags)
-                if len(diff) == 0:
-                    missing_genes[gene_group] = None
-                else:
-                    missing_genes[gene_group] = diff
-            except:
-                missing_genes[gene_group] = "Not present in current decombinator"
-
-    return missing_genes
+    try:
+        alleles_functionality, alleles_fastas = generators.get_tr_alleles_for_gene_group_for_species(gene_group, species_fmt1)
+    except FileNotFoundError:
+        return f"{gene_group} not present in current decombinator."
+    
+    imgt_gene_list = set(collapse_alleles(alleles_fastas))
+    dcr_gene_list = extract_gene_list(directory, species_fmt2, version, gene_group)
+    diff = dcr_gene_list.symmetric_difference(imgt_gene_list)
+    if len(diff) == 0:
+        return f"{gene_group} up to date."
+    else:
+        return diff
---
+++ tests/test_checker.py
@@ -0,0 +1,31 @@
+from tagbrewer.tag import checker, generators
+import pytest
+import pathlib
+
+def test_collapse_alleles():
+    alleles_dict = {"gene1": {"01": "abc", "02": "aab"},
+                    "gene2": {"01": "def", "02": "ddf"}}
+    assert checker.collapse_alleles(alleles_dict) == ["gene1", "gene2"]
+    
+class TestGeneDifferences:
+    @pytest.fixture
+    def resource_location(self):
+        return pathlib.Path("tests/resources").resolve()
+
+    def test_extract_gene_list_version_parity(self, resource_location):
+        original = checker.extract_gene_list(resource_location, "human", "original", "TRAV")
+        extended = checker.extract_gene_list(resource_location, "human", "extended", "TRAV")
+        assert original == extended
+
+    def test_extract_gene_list_extended(self, resource_location):
+        extended = checker.extract_gene_list(resource_location, "human", "extended", "TRAV")
+        assert extended == set(["TRAV1-1", "TRAV14/DV4"])
+
+    def test_find_gene_differences(self, resource_location, monkeypatch):
+        reference_gene_list = {"TRAV1-1": {"01": "abc", "02": "aab"},
+                               "TRAV14/DV4": {"01": "def", "02": "ddf"},
+                               "TRAV-2": {"01": "ghi", "02": "ggi"}}
+        monkeypatch.setattr(generators, "get_tr_alleles_for_gene_group_for_species", lambda x, y: (None, reference_gene_list))
+        differences = checker.find_gene_differences(resource_location, "human", "extended", "TRAV")
+        assert differences == set(["TRAV-2"])
+
